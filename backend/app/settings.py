62114fc255924fadbab0c5301a5e7640debc6b82
Vadim Rudenkov <greatlaki911@gmail.com>
2023-07-01 00:34:24 +0300

    Feature Swagger

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # 3d party
     "rest_framework",
+    "drf_yasg",
     # Local
     "wallets.apps.WalletsConfig",
     "users.apps.UsersConfig",
