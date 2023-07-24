a36fd042d61a67ad4b15317e7ef29429736dfd0c
greatlaki <greatlaki911@gmail.com>
2023-07-24 22:28:52 +0300

    refactoring

--- backend/wallets/views.py
+++ backend/wallets/views.py
@@ -1,5 +1,8 @@
+from json import dumps
+
 from django.db.models import Q
 from django_extended.constants import RequestMethods
+from kafka import KafkaProducer
 from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticated
 from wallets.models import Transaction, Wallet
@@ -56,6 +59,15 @@ class TransactionListCreateAPIView(generics.ListCreateAPIView):
         if user.is_admin:
             return Transaction.objects.all()
         return Transaction.objects.filter(Q(wallet__owner_id=user.pk) | Q(receiver__id=user.pk))
+
+    def post(self, request, *args, **kwargs):
+        create_transaction = self.create(request, *args, **kwargs)
+        producer = KafkaProducer(
+            bootstrap_servers=["kafka:29092"],
+            api_version=(0, 11, 5),
+            value_serializer=lambda x: dumps(x).encode("utf-8"),
+        )
+        producer.send("transactions_create", value=create_transaction)
 
 
 class TransactionRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
